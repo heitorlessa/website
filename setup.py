e02d071cf9d259a0e7d608c92d2d9c67fed28915
heitorlessa <heitor.lessa@hotmail.com>
2013-10-27 17:05:10 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='ephemeral_disk',
+      version='1.0',
+      description=("Ephemeral Disk module for AWS created for an article on HL website"),
+      long_description='Library provides functions to manage disk partitions, format, and SWAP as well as mount/umount it',
+      author='Heitor Lessa',
+      author_email='heitor.lessa@hotmail.com',
+      url='http://www.heitorlessa.com',
+      license='GPL',
+      download_url='https://github.com/heitorlessa/website.git',
+      py_modules=['ephemeral_disk']
+)
